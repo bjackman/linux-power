a7efc068fb907a88ad7b0a6993f65c223bf3fb5e
Brendan Jackman <brendan.jackman@arm.com>
2017-09-07 15:29:23 +0100

    HACK: sched/topology: Hardcode SD_ASYM_CPUCAPACITY
    This enables SD_BALANCE_WAKE

--- kernel/sched/topology.c
+++ kernel/sched/topology.c
@@ -1199,6 +1199,11 @@ sd_init(struct sched_domain_topology_level *tl,
 	return sd;
 }
 
+static int cpu_cpu_flags(void)
+{
+	return SD_ASYM_CPUCAPACITY;
+}
+
 /*
  * Topology list, bottom-up.
  */
@@ -1209,7 +1214,7 @@ static struct sched_domain_topology_level default_topology[] = {
 #ifdef CONFIG_SCHED_MC
 	{ cpu_coregroup_mask, cpu_core_flags, SD_INIT_NAME(MC) },
 #endif
-	{ cpu_cpu_mask, SD_INIT_NAME(DIE) },
+	{ cpu_cpu_mask, cpu_cpu_flags, SD_INIT_NAME(DIE) },
 	{ NULL, },
 };
 
